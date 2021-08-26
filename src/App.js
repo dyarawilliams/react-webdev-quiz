98aa9d7532f921160fcb8c9aa6107d73d22c0d3e
D'yara Williams <dyara.williams@gmail.com>
2021-08-25 20:29:34 -0400

    initial setup for app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const { isAuthenticated } = useAuth0();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Web Dev Quiz</h1>
+      {!isAuthenticated ? (
+        <div>
+          <p style={{ fontSize: "1.5rem" }}>Welcome to WebDev Scramble</p>
+          <LoginButton />
+        </div>
+      ) : (
+        <div>
+          <LogoutButton />
+          <User />
+        </div>
+      )}
     </div>
   );
 }
