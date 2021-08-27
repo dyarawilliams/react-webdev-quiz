64c7a964313eeb1f42f0b6b4e40760439bc0a0a7
D'yara Williams <dyara.williams@gmail.com>
2021-08-26 22:43:16 -0400

    added a heart to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className="footer">
-            <p>&copy; 2021 Created By D'yara Williams</p>
+            <p>&copy; 2021 Created By D'yara Williams 💖</p>
         </div>
     )
 }
